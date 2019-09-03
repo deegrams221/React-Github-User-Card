50c82cf33b62846ba5ff7e8c185cd08e20605baf
Diana Grams <deegrams221@gmail.com>
2019-09-03 16:24:13 -0600

    added in followers images and updated styling

--- github-user-card/src/Components/UserCards.js
+++ github-user-card/src/Components/UserCards.js
@@ -6,13 +6,13 @@ export default function UserCard(props) {
     <div className="User-Card">
       <img src={props.data.avatar_url} alt={props.data.name} />
       <h2>{props.data.name}</h2>
-      <a href={props.data.html}>{props.data.login}</a>
+      <a href={props.data.html}>Github:@{props.data.login}</a>
       <h3>Location: {props.data.location}</h3>
-      <h4>Followers: </h4>
+      <h4>Followers: {props.data.followers}</h4>
         <ul>
           {props.followers.map(follower => {
             return(
-              <li key={follower.login}><a href={follower.html_url}>{follower.login}</a></li>
+              <li key={follower.login}><img src={follower.avatar_url}/><a href={follower.html_url}>{follower.login}</a></li>
             )
           })}
         </ul>
